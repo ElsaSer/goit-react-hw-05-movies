762713e206470ccfdb8880727baad41e6737f148
ElsaSer <vodaelsa18@gmail.com>
2025-02-13 12:48:52 +0000

    upp

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -1,4 +1,54 @@
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+const API_KEY = 'a4e0e6c94492c515df52f4a6ebcc54c7';
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+const BASE_URL = 'https://api.themoviedb.org/3';
+
 const Cast = () => {
-  return <div>Cast!!!</div>;
+  const { movieId } = useParams();
+  const [cast, setCast] = useState([]);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchCastData = async () => {
+      try {
+        const response = await axios.get(
+          `${BASE_URL}/movie/${movieId}/credits`,
+          {
+            params: {
+              api_key: API_KEY,
+            },
+          }
+        );
+        setCast(response.data.cast);
+      } catch (error) {
+        setError('Failed to fetch cast');
+      }
+    };
+    fetchCastData();
+  }, [movieId]);
+  if (error) {
+    return <div>{error}</div>;
+  }
+  return (
+    <div>
+      <h2>Cast</h2>
+      <ul>
+        {cast.map(actor => (
+          <li key={actor.id}>
+            {actor.profile_path && (
+              <img
+                src={`${IMAGE_BASE_URL}${actor.profile_path}`}
+                alt={actor.name}
+                width={'70'}
+              />
+            )}
+            <p>{actor.name}</p>
+            <p>Character: {actor.character}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
   };
   export default Cast;
