a2e15a9f7d6d802091ce91c83f608da13447668e
ElsaSer <vodaelsa18@gmail.com>
2025-02-13 11:53:24 +0000

    c

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,18 @@ import HomePage from 'pages/HomePage';
 import MovieDetails from 'pages/MovieDetails';
 import Movies from 'pages/Movies';
 import { Routes, Route } from 'react-router-dom';
+import Cast from './Cast';
+import Reviews from './Reviews';
 export const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:movieId" element={<MovieDetails />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
       </Routes>
     </div>
   );
